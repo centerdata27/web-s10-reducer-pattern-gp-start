b8b31b5d8368c9507b76be8546ce61e3f26c01ae
centerdata27 <0KEVINDELACRUZ0@GMAIL.COM>
2025-08-06 15:19:55 -0400

    progress

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, {useReducer} from 'react'
+
+
+//what types of actions can happen top change our state 
+
+const CHANGE_Label = 'CHANGE_ LABEL'
+const CHANGE_IS_COMPLETE="CHANGE_IS_COMPLETE"
+
+//inital state value /when actions of out form get fires up (clicked)
+const initalState = {
+  todoLabel: '',
+  todoIsComplete: false
+}
+//ex.{type:CHNAGE_LABEL, payload:foo} //action
+//ex.{type: CHANGE_ISCOMPLETED, payload:true}//action
+const reducer = (state, action) => {
+
+switch (action.type){
+case CHANGE_Label:
+  return {...state, todoLabel: action.payload}
+case CHANGE_IS_COMPLETE:
+  return{...state, todoIsComplete: action.payload  }
+
+default: 
+        return state
+}
+}
 
 export default function TodoForm() {
+  const [state, dispatch] = useReducer(reducer,initialState)
+  
   return (
     <form id="todoForm">
       <h3>New Todo Form</h3>
