86b7c079429e36f061fe42a2cb6c552896d2cd42
centerdata27 <0KEVINDELACRUZ0@GMAIL.COM>
2025-08-06 23:10:35 -0400

    reducer funcs COMPLTED

--- frontend/components/TodoForm.js
+++ frontend/components/TodoForm.js
@@ -1,39 +1,74 @@
-import React, {useReducer} from 'react'
+import React, { useReducer } from 'react'
 
 
 //what types of actions can happen top change our state 
+const CHANGE_LABEL = 'CHANGE_LABEL'
+const CHANGE_IS_COMPLETE = "CHANGE_IS_COMPLETE"
 
-const CHANGE_Label = 'CHANGE_ LABEL'
-const CHANGE_IS_COMPLETE="CHANGE_IS_COMPLETE"
+
+
+
+
 
 //inital state value /when actions of out form get fires up (clicked)
-const initalState = {
+const initialState = {
   todoLabel: '',
   todoIsComplete: false
 }
+
+
+
+
 //ex.{type:CHNAGE_LABEL, payload:foo} //action
 //ex.{type: CHANGE_ISCOMPLETED, payload:true}//action
 const reducer = (state, action) => {
 
-switch (action.type){
-case CHANGE_Label:
-  return {...state, todoLabel: action.payload}
-case CHANGE_IS_COMPLETE:
-  return{...state, todoIsComplete: action.payload  }
+  switch (action.type) {
+    case CHANGE_LABEL:
+      return { ...state, todoLabel: action.payload }
+    case CHANGE_IS_COMPLETE:
+      return { ...state, todoIsComplete: action.payload }
 
-default: 
-        return state
-}
+    default:
+      return state
+  }
 }
 
-export default function TodoForm() {
-  const [state, dispatch] = useReducer(reducer,initialState)
-  
+export default function TodoForm({ createNewTodo }) {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+
+  const onLabelChange = ({ target: { value } }) => {
+    dispatch({ type: CHANGE_LABEL, payload: value })
+  }
+
+
+  const onIsCompleteChange = ({ target: { checked } }) => {
+
+    dispatch({ type: CHANGE_IS_COMPLETE, payload: checked })
+  }
+
+
+  const resetForm = () => {
+    dispatch({ type: CHANGE_LABEL, payload: "" })
+    dispatch({ type: CHANGE_IS_COMPLETE, payload: false })
+  }
+
+  const onNewToDo = evt => {
+    evt.preventDefault()
+    createNewTodo(state.todoLabel, state.todoIsComplete)
+    resetForm()
+
+
+  }
+
   return (
-    <form id="todoForm">
+    <form id="todoForm" onSubmit={onNewToDo} >
       <h3>New Todo Form</h3>
       <label><span>Todo label:</span>
         <input
+          onChange={onLabelChange}
+          value={state.todoLabel}
           type='text'
           name='todoLabel'
           placeholder='Type label'
@@ -41,6 +76,8 @@ export default function TodoForm() {
       </label>
       <label><span>Is completed:</span>
         <input
+          onChange={onIsCompleteChange}
+          checked={state.todoIsComplete}
           type='checkbox'
           name='todoIsCompleted'
         />
@@ -49,6 +86,7 @@ export default function TodoForm() {
         <input
           type='submit'
           value='Do it!'
+        // disabled={!state.todoLabel.trim()}
         />
       </label>
     </form>
